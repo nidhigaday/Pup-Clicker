e3ce4ddad379f9bf5df1e5f2e75ea00368c9ab50
Spacevash <nidhi.gaday@gmail.com>
2017-07-07 14:15:08 -0400

    Updated and reduced code version

--- clicker.js
+++ clicker.js
@@ -3,97 +3,55 @@ $(document).ready(function() {
     $('#catsValue').attr('max', allpups.length);
 });
 
-var pupNumbers, babyType;
-
 //to get no. of animals and their types and fetch it from the database
-function loadPups() {
+function loadPups(e) {
+    e.preventDefault();
+    var pupNumbers, babyType;
 
     //get the no. of puppies to display from input vallue
     pupNumbers = $('#catsValue').val();
-    console.log(pupNumbers);
 
     // checks the selected radio button and save the value of type in variable from input value
     if ($("input[type='radio'].puptype").is(':checked')) {
         babyType = $("input[type='radio'].puptype:checked").val();
     }
 
-    console.log(babyType);
-
     //main function that takes
     //number of puppies,
     //all puppies object from cat.js
     //and type of puppies from pupType
     var pupClicker = function(n, woof, typeee) {
-
-        var pupNames = [];
-        var pupImages = [];
+        var counter = 0;
 
         //loop to getall the puppies from the cat.js that matches the requirements
-        for (x = 0; x < meow.pups.length; x++) {
+        for (x = 0; x < woof.length; x++) {
 
-            var ptype = meow.pups[x].type;
-            var ctype = meow.pups[x].category;
-
-            // if the type or category or animals in cat.js matches with the input values, if pushes in a array
-            if (ptype === typeee) {
-
-                var pupname = meow.pups[x].name;
-                var pupimage = meow.pups[x].image;
-
-                pupNames.push(pupname);
-                pupImages.push(pupimage);
-
-            } else if (ctype === typeee) {
-
-                var puptname = meow.pups[x].name;
-                var puptimage = meow.pups[x].image;
-
-                pupNames.push(puptname);
-                pupImages.push(puptimage);
+            // if the type or category or animals in cat.js matches with the input values, it renders
+            if ((woof[x].type === typeee && counter < n) || (woof[x].category === typeee && counter < n)) {
+                //function call to render puppies in cats.js
+                woof[x].template();
+                counter++;
+            } else {
+                return;
             }
         }
-
-        //once array of animals that matched the requirement is collected,
-        //following loop displays the no. of animals in this array requested in input value
-        //by calling the template function to display images
-        $.each(pupNames, function(index, value) {
-            if (value !== null) {
-                for (i = 0; i < n; i++) {
-                    template(pupNames[i], pupImages[i]);
-                    console.log('rendering');
-                }
-            }
-        });
-        // for (i = 0; i < n; i++) {
-        //     if (i <= )
-        // }
     };
 
-    function template(name, image) {
-        $('.row').append('<div class="col">' +
-            '<h2>' + name + '</h2>' +
-            '<img src="' + image + '">' +
-            '<h3>No. of clicks <span>0</span></h3>' +
-            '</div>');
-    }
-
     //function call
-    pupClicker(pupNumbers, allpups, pupType);
+    pupClicker(pupNumbers, allpups, babyType);
 
-    //check if image was clicked and display the respective larger image
+    //checks if image was clicked and display the respective larger image
     //keeping this below pupclicker function call
     //because click on image is checked only once template function is done rendering images
     $('.cat-list img').click(function() {
         var index = $(this).parent().index();
-        $('#large-image').attr('src', allpups.pups[index].image);
+        $('#large-image').attr('src', allpups[index].image);
         $(this).parent().find('span').text(function() {
             var clicks = parseInt($(this).text(), 10) + 1;
             return clicks;
         });
     });
-
     return false;
-
 }
 
 //function to reset all the values
@@ -104,18 +62,5 @@ function resetPage() {
 }
 
 //calling function to loadcats
-$('#catCount').submit(loadPups);
+$('input:submit').click(loadPups);
 $('input:reset').click(resetPage);
-
-
-// Function in Vanilla JS
-// var elem = document.getElementById('Catclicks');
-// console.log(elem);
-// var elemTwo = document.getElementById('CatclicksTwo');
-// console.log(elemTwo);
-// function Click(){
-//  count++;
-//  document.getElementById('ClickCount').innerHTML = "No. of clicks " + count;
-// };
-// elem.addEventListener('click', Click, false);
-// elemTwo.addEventListener('click', Click, false);
