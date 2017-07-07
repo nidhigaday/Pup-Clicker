7894bba92bdf0d6ecdfe2950ae285f9834537fd1
Spacevash <nidhi.gaday@gmail.com>
2017-07-07 14:12:28 -0400

    Updated Cat.js
    replaced repeated code with prototypal class method

--- cats.js
+++ cats.js
@@ -1,18 +1,31 @@
 //class function for all the animals
 var BabyAnimal = function(name, path, typee) {
-	var obj = {
-		name: name,
-		image: path,
-		type: typee,
-		category: 'both'
-	};
-	return obj;
+
+	//used Object.create() method to enable prototype delegation
+	//if called BabyAnimal lookup to BabyAnimal.prototype for template function
+
+	//var this = Object.create(BabyAnimal.prototype); -----> done this by keyword 'new' pseudoclassical pattern
+
+	this.name = name;
+	this.image = path;
+	this.type = typee;
+	this.category = 'both';
+
+	//return this; ----> done this by keyword 'new'
+};
+
+BabyAnimal.prototype.template = function() {
+    return $('.row').append('<div class="col">' +
+            '<h2>' + this.name + '</h2>' +
+            '<img src="' + this.image + '">' +
+            '<h3>No. of clicks <span>0</span></h3>' +
+            '</div>');
 };
 
 //array to store all the animal objects
 var allpups = [];
 
-//all new animals created using class function
+//all  animals created using class function
 allpups.push(new BabyAnimal('Rufus', 'images/puppy.JPG', 'dog'));
 allpups.push(new BabyAnimal('Tina', 'images/Cat.jpg', 'cat'));
 allpups.push(new BabyAnimal('Oscar', 'images/puppyTwo.PNG', 'dog'));
@@ -25,5 +38,6 @@ allpups.push(new BabyAnimal('Mikky & Ginny', 'images/puppyFive.JPG', 'dog'));
 allpups.push(new BabyAnimal('Mike', 'images/CatFive.jpg', 'cat'));
 allpups.push(new BabyAnimal('Mau & Pau', 'images/puppySix.jpg', 'dog'));
 
-console.log(allpups.length);
-console.log(allpups);
+// console.log(allpups.length);
+// console.log(allpups);
+// console.log(allpups[1].template());
